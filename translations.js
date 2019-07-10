ab8a211d1de640d9872a7f510544c03c4470740a
claire <52763197+clairemin@users.noreply.github.com>
2019-07-10 15:15:46 -0700

    Add files via upload

---
+++ translations.js
@@ -0,0 +1,33 @@
+
+// function myFunction()
+//   {alert("Page has been loaded :)");}
+
+// <link rel = "stylesheet" href = "mystyle.css">
+
+
+// function changePhoto() {
+//   // console.log("within changePhoto");
+//   var pinky = "https://pbs.twimg.com/profile_images/1139998624207818753/Zs-nZe9F_400x400.jpg";
+//   var family = "https://media.tenor.com/images/a7d1702cd53d57eb574670d50715cb7f/tenor.gif";
+//   // var family = "https://img.sbs.co.kr/newimg/news/20190418/201304537_1280.jpg";
+//
+//   document.getElementById('picture').src = 'family';
+  // document.getElementById('picture')
+
+  // <button onclick = "document.getElementById('picture').src = 'pink'">button1</button>;
+
+  // img id = "picture";
+  // src = "family";
+
+  // <button onclick = "document.getElementById('picture').src = family" class = "smaller">button2</button>;
+
+
+  function changePhoto() {
+    var family = "giffy.gif";
+    document.getElementById('picture').src = family;
+    }
+
+  function enlarge() {
+
+    document.getElementById('btstrans.jpeg').style.display = 'block'
+  }
